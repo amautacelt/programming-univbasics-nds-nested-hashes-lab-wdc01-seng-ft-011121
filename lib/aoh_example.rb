60226d2968432de7ebbadb71da2f9ad91885ae2e
C. L <amautacelt@gmail.com>
2020-12-29 05:18:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,15 +18,6 @@ def assembled_aoh
   { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
   
-  result = [
-  { :name =>  "Don Gately", :occupation => "Live-in Staff" },
-  { :name =>  "Joelle van Dyne",:occupation => "Radio Personality" },
-  { :name =>  "Pat Monteseian", :occupation => "Staff" },
-  { :name =>  "Kate Gompert", :occupation => "None" },
-  { :name =>  "Bruce Green", :occupation => "Fan of Mildred" }
-  ]
-  
-result
 end
 
 def literal_aoh
